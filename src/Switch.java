15de14d392fe4edb158c7c3e5f499fff9081d502
Thando Mini <tzone85@gmail.com>
2017-01-11 15:11:42 +0200

    made simple if and switch statemnets

--- src/Switch.java
+++ src/Switch.java
@@ -3,6 +3,34 @@
  */
 public class Switch {
     public static void main(String [] args){
+        int value = 1;
 
+        if(value == 1){
+            System.out.println("Value was 1");
+        }else if(value == 2){
+            System.out.println("Value was 2");
+        }else{
+            System.out.println("Value was 1 or 2");
+        }
+
+        int switchValue = 1;
+
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3:
+                System.out.println("value was 3");
+                break;
+
+            default:
+                System.out.println("Value was 1, 2 or 3");
+                break;
+        }
     }
 }
